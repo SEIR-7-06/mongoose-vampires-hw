3a2833e23887b166d7436d97b39323a374486843
cotsao <codytsao@gmail.com>
2021-08-04 11:44:21 -0700

    made a schema

---
+++ models/vampire.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
+
+const vampireSchema = new Schema({
+    name: { type:String, required: true},
+    hair_color: {type:String, default:'blonde'},
+    eye_color: {type: String},
+    dob: {type: Date},
+    loves: [{type:String}],
+    location: {type:String},
+    gender: {type:String},
+    victims: {
+        type:Number,
+        min:0,
+        required:true
+    }
+})
+
+
+const Vampire = mongoose.model('Vampire',vampireSchema)
+module.exports = Vampire
