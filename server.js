bf7b0bfdb3c9a264adb568604e15949fd3f3f251
off2mxrs <off2mxrs@gmail.com>
2021-08-06 17:00:31 -0700

    Error accessing model

--- server.js
+++ server.js
@@ -1,16 +1,52 @@
 // 1. Require Mongoose
 const mongoose = require('mongoose')
 // 2. Require your Model
-const monVampire = require('./vampire')
+const db = require('./models/vampire')
 // 3. Require your extra data source
 
 // 4. Connect your database
 
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
-db.Vampire.create(VampireData, (err, addVampires) => {
+db.Vampire.create({
+    name: 'newCount',
+    hair_color: 'blonde',
+    eye_color: 'brown',
+    dob: new Date(1571, 2, 13, 7, 47),
+    loves: ['cereal','marshmallows'],
+    location: 'Minneapolis, Minnesota, US',
+    gender: 'm',
+    victims: 2,
+  },{
+    name: 'newJoker',
+    hair_color: 'green',
+    eye_color: 'brown',
+    dob: new Date(171, 2, 15, 7, 47),
+    loves: ['robin'],
+    location: 'gotham',
+    gender: 'm',
+    victims: 300,
+  },{
+    name: 'newHarley Quinn',
+    hair_color: 'blonde',
+    eye_color: 'black',
+    dob: new Date(1990, 7, 17, 7, 47),
+    loves: ['hammers','joker'],
+    location: 'Gotham',
+    gender: 'f',
+    victims: 88,
+  },{
+    name: 'newAlucard',
+    hair_color: 'silver',
+    eye_color: 'brown',
+    dob: new Date(1671, 8, 22, 3, 50),
+    loves: ['Dracula'],
+    location: 'Castlevania',
+    gender: 'f',
+    victims: 1000,
+  }, (err, createVampires) => {
     if(err) return console.log(err);
-    console.log(addVampires)
+    console.log(createVampires)
 })
 
 /////////////////////////////////////////////////
@@ -60,3 +96,5 @@ db.Vampire.create(VampireData, (err, addVampires) => {
 //## Negative Selection
 
 /////////////////////////////////////////////////
+module.exports = monVampire
+module.exports = mongoose
