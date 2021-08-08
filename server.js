6143d43be82ec915807a3d3f677a95c6faeb9736
nndeto <n_ndeto@yahoo.com>
2021-08-08 12:36:49 -0400

    added my own vamp data to db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // 1. Require Mongoose
 const mongoose = require('mongoose');
 const Vampire = require('./models/Vampire.js');
+// const Vampire = require('./models/Vampire.js');
 
 // 2. Require your Model
 module.exports = {
@@ -33,12 +34,52 @@ mongoose.connection.on('connected', () => {
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-Vampire.collection.insertMany(vampData, (err, data) => {
-    if (err) return console.log(err);
-    console.log(data)
-    mongoose.connection.close();
-})
+// Vampire.collection.insertMany(vampData, (err, data) => {
+//     if (err) return console.log(err);
+//     console.log(data)
+//     mongoose.connection.close();
+// })
 // ### Add some new vampire data
+// Vampire.create({
+// 	name: 'Luna Rey',  
+// 	hair_color: 'purple', 
+// 	eye_color: 'black',  
+// 	dob: (1610, 06, 16), 
+// 	loves: ['writing', 'reading', 'crying'], 
+// 	location: 'Portland, Maine', 
+// 	gender: 'f', 
+// 	victims: 186
+// })
+Vampire.create({
+	name: 'Astrid Blair',  
+	hair_color: 'silver', 
+	eye_color: 'green',  
+	dob: (110, 02, 22), 
+	loves: ['glass blowing', 'snowboarding', 'hiking', 'hot chocolate'], 
+	location: 'Swiss Alps', 
+	gender: 'f', 
+	victims: 4
+})
+Vampire.create({
+	name: 'Julian Frost',  
+	hair_color: 'black', 
+	eye_color: 'amber',  
+	dob: (2050, 10, 3), 
+	loves: ['helping people', 'forging weapons', 'visiting friends', 'travelling'], 
+	location: 'Kyoto, Japan', 
+	gender: 'm', 
+	victims: 999
+})
+Vampire.create({
+	name: 'Gio Galileo',  
+	hair_color: 'red', 
+	eye_color: 'black',  
+	dob: (1946, 12, 1), 
+	loves: ['hiding', 'motorcycles', 'meat'], 
+	location: 'unknown', 
+	gender: 'm', 
+	victims: 420
+})
 
 /////////////////////////////////////////////////
 // ## QUERYING
