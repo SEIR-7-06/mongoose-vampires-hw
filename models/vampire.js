3b2fc6ed015d326e2837ccaa019d975b2fa8f1a5
erininabox <erininabox@gmail.com>
2021-08-07 21:07:32 -0700

    fixed syntax errer, all tests pass

--- models/vampire.js
+++ models/vampire.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
 // Set the Schema
-const lovesSchema = new mongoose.Schema({
-    type: Array,
-});
 const vampireSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    hair_color: { type: String, default: 'blonde', },
-    eye_color: {type: String, },
-    dob: { type: Date, },
-    loves: [lovesSchema],
+    hair_color: { type: String, default: 'blonde' },
+    eye_color: String,
+    dob: Date,
+    loves: [String],
     location: String,
     gender: String,
     victims: { type: Number, min: 0 }
